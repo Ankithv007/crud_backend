5ed65c10b4af461a77084cd45de4056e36305e43
Ankith B V <Ankith@VTPPL.COM>
2025-06-05 08:54:49 +0530

    initial push for the backend

--- client/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: './',  // <-- Important if you see white screen on reload
-  plugins: [react()]
-})
